60286877a8f3f6d19a068273723347793161476a
evynaranjo <evy_naranjo@icloud.com>
2026-09-18 21:21:29 -0600

    [Add] Page Added

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'coin-quest-v4';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+        }
+        return response;
+      }).catch(() =>
+        event.request.mode === 'navigate' ? caches.match('./index.html') : Response.error()
+      )
+    )
+  );
+});
